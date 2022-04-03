d43738efe6e675854c1eb17d275641da60d99ccb
Ashish Shinde <ashishshinde821@gmail.com>
2022-04-03 12:10:13 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BuilderComponent } from './builder/builder.component';
+import { NextDirective } from './next.directive';
+import { PrevDirective } from './prev.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BuilderComponent,
+    NextDirective,
+    PrevDirective
   ],
   imports: [
     BrowserModule,
